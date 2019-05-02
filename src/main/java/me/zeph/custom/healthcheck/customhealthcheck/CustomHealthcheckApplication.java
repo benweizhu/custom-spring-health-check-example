ddad7bbd5e7c3542897390954ae6a4c8ab18c368
Benwei Zhu <benweizhu@gmail.com>
2019-05-02 10:34:16 +0800

    init

---
+++ src/main/java/me/zeph/custom/healthcheck/customhealthcheck/CustomHealthcheckApplication.java
@@ -0,0 +1,13 @@
+package me.zeph.custom.healthcheck.customhealthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomHealthcheckApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CustomHealthcheckApplication.class, args);
+  }
+
+}
---
+++ src/main/java/me/zeph/customspringhealthcheck/CustomSpringHealthCheckExampleApplication.java
@@ -0,0 +1,13 @@
+package me.zeph.customspringhealthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomSpringHealthCheckExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CustomSpringHealthCheckExampleApplication.class, args);
+  }
+
+}
